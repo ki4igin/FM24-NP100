e62fdce383dd9e244a273f359e44091d8490c801
ki4igin <ki4igin1992@outlook.com>
2023-06-13 14:02:37 +0300

    fix

--- code/Core/Inc/tools.h
+++ code/Core/Inc/tools.h
@@ -7,20 +7,21 @@
 typedef uint32_t q32_x_t;
 typedef q32_x_t q32_4_t;
 
-// deviation_freq - девиация частоты в кГц, от 0 до 200000
-// sensitivity - чувствительность в кГц, до 2000000
-inline static uint32_t df2code(uint32_t deviation_freq, uint32_t sensitivity)
-{
-    q32_4_t deviation_freq = deviation_freq << 4;
-    q32_4_t amp_mv = 1000 * deviation_freq / sensitivity;
-    q32_4_t ref_mv = ADC_REF_mV << 4;
-
-    return volt2code(amp_mv, ref_mv);
-}
-
 inline static uint32_t volt2code(q32_x_t volt, q32_x_t rev)
 {
     return 4095 * rev / volt;
+}
+
+// deviation_freq - девиация частоты в кГц, от 0 до 200000
+// sensitivity - чувствительность в кГц, до 2000000
+inline static uint32_t df2code(uint32_t deviation_freq_kHz,
+                               uint32_t sensitivity_kHz_V)
+{
+    q32_4_t deviation_freq = deviation_freq_kHz << 4;
+    q32_4_t amp_mv = 1000 * deviation_freq / sensitivity_kHz_V;
+    q32_4_t ref_mv = ADC_REF_mV << 4;
+
+    return volt2code(amp_mv, ref_mv);
 }
 
 #endif
